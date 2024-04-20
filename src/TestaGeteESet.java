106221d28f8ea393020749d49e69fb1c0c49e411
Leandro de Souza Santos <leandrosouza@id.uff.br>
2024-04-20 18:12:07 -0300

    Primeiro commit

---
+++ src/TestaGeteESet.java
@@ -0,0 +1,7 @@
+public class TestaGeteESet {
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        conta.setNumero(1336);
+        System.out.println(conta.getNumero());
+    }
+}
