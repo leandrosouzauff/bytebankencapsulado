38618922b8b80f4cd75ccd3ab765ac0febdec3b2
Leandro de Souza Santos <leandrosouza@id.uff.br>
2024-04-20 18:36:28 -0300

    Adicionando Getters e Setters

--- src/TestaGeteESet.java
+++ src/TestaGeteESet.java
@@ -3,5 +3,16 @@ public class TestaGeteESet {
         Conta conta = new Conta();
         conta.setNumero(1336);
         System.out.println(conta.getNumero());
+        Cliente leandro = new Cliente();
+        leandro.setNome("Leandro de Souza Santos");
+        conta.setTitular(leandro);
+        System.out.println(conta.getTitular().getNome());
+        conta.getTitular().setProfissao("Analista de TI");
+        // agora em duas linhas
+        Cliente titularDaConta = conta.getTitular();
+        titularDaConta.setProfissao("Analista de TI");
+        System.out.println(titularDaConta);
+        System.out.println(leandro);
+        System.out.println(conta.getTitular());
     }
 }
